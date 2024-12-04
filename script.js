f3ac6ffe273efc218349952fbea8304ebd57baed
Jhovana <jhovanapatty95@gmail.com>
2024-12-04 13:21:44 -0800

    se agrego una pagina de reproducir video

---
+++ script.js
@@ -0,0 +1,13 @@
+function ver(){
+    alert("estas viendo minions")
+}
+function info(){
+    alert("apto para todo publico")
+}
+
+
+// alert("hola mundo")
+// ver()
+function kenny(){
+    alert("hola soy kenny")
+}
